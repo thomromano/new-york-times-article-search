abbea0e4076b3441942bdcd86c3314f5bc30602b
wdarsow <wade.darsow@gmail.com>
2018-03-20 21:04:42 -0500

    updated app.js, AJAX call is working, ability to see individual fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,23 +9,26 @@ $('document').ready(function(){
 $('#searchButton').click(function() {
     searchParm = $('#searchid1').val();
     console.log(searchParm);
-})
+    let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=402206c1ad1e4b02a94655a84f0d41ce&q=" + searchParm;
+    
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
 
 
-console.log(searchParm);
 
-let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=402206c1ad1e4b02a94655a84f0d41ce&q=burglary"
-
+  })
 
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+
+
+
+
 
 
 
