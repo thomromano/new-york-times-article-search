27ecac626f594278e43ae95caf7a0eef326d3c40
wdarsow <wade.darsow@gmail.com>
2018-03-20 20:08:34 -0500

    app.js changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+
+//url += '?' + $.param({
+//  'api-key': "402206c1ad1e4b02a94655a84f0d41ce"
+//});
+
+url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=402206c1ad1e4b02a94655a84f0d41ce&q=burglary"
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
